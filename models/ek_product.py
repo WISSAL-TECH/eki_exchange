f7b7529c40eb6e9310ec42bdc09e9472afa46dc1
oumaima <messai.oumaima28@gmail.com>
2024-01-16 11:08:52 +0100

    update

--- models/ek_product.py
+++ models/ek_product.py
@@ -81,7 +81,7 @@ class Product(models.Model):
 
             return rec
 
-    @api.model
+    @api.multi
     def write(self, vals):
         _logger.info(
             '\n\n\n update\n\n\n\n--->>  %s\n\n\n\n', vals)
