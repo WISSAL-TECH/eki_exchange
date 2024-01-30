2282b49eefbcdb4b3e1e40b9a1e37db7635219df
oumaima <messai.oumaima28@gmail.com>
2024-01-30 13:54:36 +0100

    generate default code

--- models/ek_product.py
+++ models/ek_product.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     create_by = fields.Char(string="Créé à partir de",readonly=True)
     brand = fields.Many2one("product.brand", 'Marque')
     image_url = fields.Char(string='Image URL')
+    manufacture_name = fields.Char(string='Fabricant')
 
     def generate_code(self):
         """Generating default code for ek products"""
@@ -92,29 +93,6 @@ class Product(models.Model):
 
             rec = super(Product, self).create(vals)
 
-            product_json = {
-                "name": rec.name,
-                "description": rec.description,
-                "categoryName": rec.categ_id.name,
-                "brand": {
-                    "name": rec.brand_id.name,
-                    "reference": ""
-                },
-                "refConstructor": rec.default_code,
-                "manufactureName": "",
-                "activate": True,
-                "configurations": []
-            }
-            configurations = []
-
-            product_json["configurations"] = configurations
-            _logger.info(
-                    '\n\n\n PRODUCT BODY JSON\n\n\n\n--->>  %s\n\n\n\n', product_json)
-            response = requests.post(domain + url_product, data=json.dumps(product_json),
-                                     headers=self.headers)
-            _logger.info('\n\n\n(CREATE product) response from eki \n\n\n\n--->  %s\n\n\n\n',
-                         response.content)
-
             return rec
 
     def write(self, vals):
@@ -169,3 +147,68 @@ class Product(models.Model):
            rec = super(Product, self).write(vals)
 
            return rec
+
+class EkiProduct(models.Model):
+    _inherit = ['product.product']
+
+    create_by = fields.Char(string="Créé à partir de",readonly=True)
+    image_url = fields.Char(string='Image URL')
+    manufacture_name = fields.Char(string='Fabricant')
+
+    def generate_code(self):
+        """Generating default code for ek products"""
+
+        # Generate a random 6-digit number
+        random_number = random.randint(100000, 999999)
+
+        # Concatenate the "eki_" and random number
+        product_code = f"eki_{random_number}"
+
+        return product_code
+
+
+    # set the url and headers
+    headers = {"Content-Type": "application/json", "Accept": "application/json", "Catch-Control": "no-cache"}
+
+    @api.model
+    def create(self, vals):
+        logging.warning("create product ======")
+        logging.warning(vals)
+        # 1- CREATE A PRODUCT FROM ekiclik
+        domain = "https://ekiclik.admin.wissal-group.com"
+        url_product = "/api/odoo/products"
+
+        if 'create_by' in vals.keys() and vals['create_by'] == 'Odoo':
+        #  CREATE A PRODUCT FROM ODOO (Send a product to Imtech)
+            vals["default_code"] = self.generate_code()
+            if "image_url" in vals and vals["image_url"]:
+                image = base64.b64encode(requests.get(vals["image_url"]).content)
+                vals["image_1920"] = image
+                vals.pop("image_url")
+
+            rec = super(EkiProduct, self).create(vals)
+
+            product_json = {
+                "name": rec.name,
+                "description": rec.description,
+                "categoryName": rec.categ_id.name,
+                "brand": {
+                    "name": rec.brand_id.name,
+                    "reference": ""
+                },
+                "refConstructor": rec.default_code,
+                "manufactureName": "",
+                "activate": True,
+                "configurations": []
+            }
+            configurations = []
+
+            product_json["configurations"] = configurations
+            _logger.info(
+                    '\n\n\n PRODUCT BODY JSON\n\n\n\n--->>  %s\n\n\n\n', product_json)
+            response = requests.post(domain + url_product, data=json.dumps(product_json),
+                                     headers=self.headers)
+            _logger.info('\n\n\n(CREATE product) response from eki \n\n\n\n--->  %s\n\n\n\n',
+                         response.content)
+
+            return rec
