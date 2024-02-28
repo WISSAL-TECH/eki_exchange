ee181221c790f45a5852f23c371086f610f36022
oumaima <messai.oumaima28@gmail.com>
2024-02-28 16:25:47 +0100

    creating pos

--- models/ek_company.py
+++ models/ek_company.py
@@ -6,6 +6,8 @@ _logger = logging.getLogger(__name__)
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
+    create_by = fields.Char()
+
     def create(self, vals):
         if 'create_by' in vals and vals['create_by']!= 'odoo':
             logging.warning("create pos from ekiclik ======")
