727250eacd23eaed38eba9f3979e8661c5f4194e
oumaima <messai.oumaima28@gmail.com>
2024-04-15 11:55:16 +0100

    fixing user

--- models/ek_user.py
+++ models/ek_user.py
@@ -31,37 +31,44 @@ class ResUsers(models.Model):
     @api.model
     def create(self, vals):
         rec = super(ResUsers, self).create(vals)
+        if "ek_user" in vals and vals.get('ek_user')== True:
 
-        logging.warning("create user ======")
-        logging.warning(vals)
+            logging.warning("create user ======")
+            logging.warning(vals)
 
-        domain = ""
-        domain_cpa = ""
-        config_settings = self.env['res.config.settings'].search([], order='id desc', limit=1)
-        if config_settings:
-            domain = config_settings.domain
-            domain_cpa = config_settings.domain_cpa
-            url_users = "/api/odoo/users"
+            domain = ""
+            domain_cpa = ""
+            config_settings = self.env['res.config.settings'].search([], order='id desc', limit=1)
+            if config_settings:
+                domain = config_settings.domain
+                domain_cpa = config_settings.domain_cpa
+                url_users = "/api/odoo/users"
 
-        data = {
-            "username": vals.get('name'),
-            "first_name": vals.get('first_name'),
-            "last_name": vals.get('last_name'),
-            "phone": vals.get('phone'),
-            "addresse": vals.get('addresse'),
-            "codification": vals.get('codification'),
-            "roles": vals.get('roles')
-        }
+            data = {
+                "username": vals.get('name'),
+                "first_name": vals.get('first_name'),
+                "last_name": vals.get('last_name'),
+                "phone": vals.get('phone'),
+                "addresse": vals.get('addresse'),
+                "codification": vals.get('codification'),
+                "roles": vals.get('roles')
+            }
 
-        _logger.info('\n\n\n D A T A \n\n\n\n--->>  %s\n\n\n\n', data)
+            _logger.info('\n\n\n D A T A \n\n\n\n--->>  %s\n\n\n\n', data)
 
-        response_cpa = requests.post(str(domain_cpa) + str(url_users), data=json.dumps(data),
+            response_cpa = requests.post(str(domain_cpa) + str(url_users), data=json.dumps(data),
+                                         headers=self.headers)
+            _logger.info('\n\n\n(CREATE user) response from cpa\n\n\n\n--->  %s\n\n\n\n', response_cpa.content)
+
+            response = requests.post(str(domain) + str(url_users), data=json.dumps(data),
                                      headers=self.headers)
-        _logger.info('\n\n\n(CREATE user) response from cpa\n\n\n\n--->  %s\n\n\n\n', response_cpa.content)
-
-        response = requests.post(str(domain) + str(url_users), data=json.dumps(data),
-                                 headers=self.headers)
-        _logger.info('\n\n\n(CREATE user) response from alsalam \n\n\n\n--->  %s\n\n\n\n', response.content)
+            _logger.info('\n\n\n(CREATE user) response from alsalam \n\n\n\n--->  %s\n\n\n\n', response.content)
 
 
-        return rec
+            return rec
+
+        else:
+
+            return rec
+
+
