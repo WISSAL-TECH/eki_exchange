083ce92d0f8696550f28a8a0747e470c8d96779d
oumaima <messai.oumaima28@gmail.com>
2024-04-19 15:38:46 +0100

    update user on pos

--- models/ek_company.py
+++ models/ek_company.py
@@ -70,7 +70,7 @@ class ResCompany(models.Model):
             address_pos = vals.get('street') or self.mapped('street')
             pos_phone_one = vals.get('phone') or self.mapped('phone')
             pos_phone_two = vals.get('mobile') or mobile
-            codification = vals.get('codification') if 'codification' in vals else self.codification,
+            codification = vals.get('codification', self.codification) if 'codification' in vals else self.codification
             source = vals.get('source') or self.mapped('source')
 
             body["params"]["data"]["name_pos"] = name_pos
