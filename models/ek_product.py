f3a419c09292068b2b212797da1c19133663890c
oumaima <messai.oumaima28@gmail.com>
2024-04-23 23:08:06 +0100

    update ref odoo

--- models/ek_product.py
+++ models/ek_product.py
@@ -498,18 +498,20 @@ class EkiProduct(models.Model):
 
     @api.model
     def create(self, vals):
-        # Appeler la méthode de création de la classe parente
+        # Generate the name for the product variant
+        values = []
         ref = self.generate_code()
         vals['reference'] = self.generate_code()
         _logger.info('\n\n\n creating variante vals\n\n\n\n--->  %s\n\n\n\n', vals)
+        for value in vals.get('product_template_attribute_value_ids', []):
+            values.append(value.get('name'))
+        name = self.generate_name_variante(vals.get('name'), vals.get('reference'), values)
+
+        # Update the name field in the values dictionary
+        vals['name'] = name
+
+        # Call the create method of the parent class
         rec = super(EkiProduct, self).create(vals)
-        _logger.info('\n\n\n product created\n\n\n\n--->  %s\n\n\n\n', vals)
-        rec.write({'reference': ref})
-        values = []
-        for value in rec.product_template_attribute_value_ids:
-            values.append(value.name)
-        name = self.generate_name_variante(rec.name, rec.constructor_ref, values)
-        rec.write({'name': name})
 
         return rec
 
