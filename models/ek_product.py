e8e6552e243936fc479d02be21c9272afce49de4
oumaima <messai.oumaima28@gmail.com>
2024-04-30 14:35:55 +0100

    Revert "UPDATE NAME"
    This reverts commit 979cb9e620c08059da69e53588178c6b7c550e84.

--- models/ek_product.py
+++ models/ek_product.py
@@ -436,7 +436,7 @@ class EkiProduct(models.Model):
             # Update the product record with the S3 image URL
             return s3_url
 
-    @api.depends('name_store', 'name')
+    @api.onchange('name_store')
     def _onchange_name(self):
         self.name = self.name_store
     @api.depends('ref_odoo')
