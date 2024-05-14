8e3c24e4081571f91f97f55c9206890fdee3ea3c
oumaima <messai.oumaima28@gmail.com>
2024-05-14 17:59:00 +0100

    sending prices

--- models/ek_product.py
+++ models/ek_product.py
@@ -741,10 +741,8 @@ class EkiProduct(models.Model):
                     price = rec.standard_price + taxe
 
                 marge2 = (rec.standard_price * 61) / 100
-                rec.prix_ek = price + marge2
                 vals['prix_ek'] = price + marge2
                 marge1 = (rec.standard_price * 11.1) / 100
-                rec.prix_central = price + marge1
                 vals['prix_central'] = price + marge1
 
             # Check if 'reference' key exists in vals, if not, use rec.reference
